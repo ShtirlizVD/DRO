a97a69f1f9bc4669e6775175447b8b6788afe24f
Z User <z@container>
2026-03-04 10:10:19 +0000

    Fix: add back AlertDialog import

--- app/src/main/java/com/dro/lathe/MainActivity.java
+++ app/src/main/java/com/dro/lathe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dro.lathe;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
